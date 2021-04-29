f46aa647a8afc2b8958474bc3922cd6b8e4dd0cf
Alfred <ya95123@gmail.com>
2021-04-29 17:59:14 +0800

    TODO

--- 210429_intro/getData.php
+++ 210429_intro/getData.php
@@ -2,9 +2,8 @@
 //修改
 $json_string = file_get_contents("data.json"); // 從檔案中讀取資料到PHP變數
 // var_dump($json_string);
+
 $data = json_decode($json_string, true); // 把JSON字串轉成PHP陣列
 $data["url"] = $_POST['url'];
 $json_strings = json_encode($data);
 file_put_contents("data.json", $json_strings); //寫入
-
-// var_dump($_POST['url']);
