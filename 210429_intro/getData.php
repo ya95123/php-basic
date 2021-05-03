0be0d7922c6bb695c33ac8d01c47b256dcfdcf73
Alfred <ya95123@gmail.com>
2021-05-03 11:40:38 +0800

    test OK

--- 210429_intro/getData.php
+++ 210429_intro/getData.php
@@ -4,6 +4,6 @@ $json_string = file_get_contents("data.json"); // 從檔案中讀取資料到PHP
 // var_dump($json_string);
 
 $data = json_decode($json_string, true); // 把JSON字串轉成PHP陣列
-$data["url"] = $_POST['url'];
+$data["url"] = $_POST["url"];
 $json_strings = json_encode($data);
 file_put_contents("data.json", $json_strings); //寫入
